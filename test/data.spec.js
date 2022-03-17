c8bbc5fee91768bb775fedb6322d65a83c380fd9
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-03-17 16:00:34 -0300

    mudando o nome da funcao nos testes

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,37 +1,37 @@
-import { buscarNome, buscarEspecie, sortOrdem, calculos} from '../src/data.js';
+import { buscarNome, buscarEspecie, calculos, ordenacao } from '../src/data.js';
 
 const dados = [
-   {
-      "name": "Bearded Lady",
-      "status": "Dead",
-      "species": "Alien",
-      "gender": "Female",
-    },
-    {
-      "name": "Boobloosian",
-      "status": "Dead",
-      "species": "Alien",
-      "gender": "unknown",
-    },
-    {
-      "name": "Rick Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "gender": "Male",
-    },
-    {
-      "name": "Morty Smith",
-      "status": "Alive",
-      "species": "Human",
-      "gender": "Male",
-    }
+  {
+    "name": "Bearded Lady",
+    "status": "Dead",
+    "species": "Alien",
+    "gender": "Female",
+  },
+  {
+    "name": "Boobloosian",
+    "status": "Dead",
+    "species": "Alien",
+    "gender": "unknown",
+  },
+  {
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+  },
+  {
+    "name": "Morty Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+  }
 ];
 
 const nomes = [
-  {"name": "Bearded Lady"},
-  {"name": "Rick Sanchez"},
-  {"name": "Morty Smith"},
-  {"name": "Boobloosian"},
+  { "name": "Bearded Lady" },
+  { "name": "Rick Sanchez" },
+  { "name": "Morty Smith" },
+  { "name": "Boobloosian" },
 ]
 
 /////////////////////////TESTE PARA CAMPO DE BUSCA NOME
@@ -41,52 +41,52 @@ describe('buscarNome', () => {
   });
   it('Deverá buscar por um nome', () => {
     const expected = buscarNome(nomes, 'banana')
-    expect (expected).toEqual([])
+    expect(expected).toEqual([])
   });
   it('Deverá buscar por um nome', () => {
     const expected = buscarNome(nomes, 'Rick')
-    expect (expected).toEqual([{"name": "Rick Sanchez"}])
+    expect(expected).toEqual([{ "name": "Rick Sanchez" }])
   });
 });
 
 /////////////////////TESTE PARA FILTRAR A ESPÉCIE 
 describe('buscarEspecie', () => {
   it('Deverá ser uma função', () => {
-  expect(typeof buscarEspecie).toBe('function');
-});
+    expect(typeof buscarEspecie).toBe('function');
+  });
 
-it('Deverá filtrar pela espécie Alien', () => {
-  const especieAlien = "Alien"
-  const expected = buscarEspecie(dados, especieAlien)
-  expect (expected.length).toEqual(2)
-  expect (expected[0].species).toEqual(especieAlien)
-  expect (expected[1].species).toEqual(especieAlien)
+  it('Deverá filtrar pela espécie Alien', () => {
+    const especieAlien = "Alien"
+    const expected = buscarEspecie(dados, especieAlien)
+    expect(expected.length).toEqual(2)
+    expect(expected[0].species).toEqual(especieAlien)
+    expect(expected[1].species).toEqual(especieAlien)
   });
 });
 
 /////////////////////TESTE PARA ORDENAÇÃO ALFABÉTICA
-describe('sortOrdem', () => {
+describe('ordenacao', () => {
   it('Deverá ser uma função', () => {
-  expect(typeof sortOrdem).toBe('function');
-});
+    expect(typeof ordenacao).toBe('function');
+  });
 
   it('Deverá retornar em ordem A-Z', () => {
-    expect(sortOrdem(nomes, "a-z")).toEqual([nomes[0], nomes[3], nomes[2], nomes[1]]);
+    expect(ordenacao(nomes, "a-z")).toEqual([nomes[0], nomes[3], nomes[2], nomes[1]]);
   });
   it('Deverá retornar em ordem Z-A', () => {
-    expect(sortOrdem(nomes, "z-a")).toEqual([nomes[1], nomes[2], nomes[3], nomes[0]]);
+    expect(ordenacao(nomes, "z-a")).toEqual([nomes[1], nomes[2], nomes[3], nomes[0]]);
   });
 });
 
 /////////////////////TESTE PARA CALCULAR A PORCENTAGEM
 describe('calculos', () => {
   it('Deverá ser uma função', () => {
-  expect(typeof calculos).toBe('function');
-});
+    expect(typeof calculos).toBe('function');
+  });
 
-it('Deverá retornar 50% de personagens masculinos', () => {
-  const expected = calculos(dados.length, 2)
-  expect (expected).toEqual(50)
+  it('Deverá retornar 50% de personagens masculinos', () => {
+    const expected = calculos(dados.length, 2)
+    expect(expected).toEqual(50)
   });
 });
 
